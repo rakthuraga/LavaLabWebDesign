e061baa07d9a1435c7a2dd6d1685e777453fb2e0
rakthuraga <thuraga@usc.edu>
2024-02-03 20:18:40 -0800

    Lebron James

--- script.js
+++ script.js
@@ -15,9 +15,14 @@ function alertMessage() {
 }
 
 function alertMessageBack() {
-  alert("No.");
+  alert("No! Click Finish Now!");
 }
 
 function alertRandomLady() {
   alert("Idk who this random lady is tbh.");
+}
+
+function redirectToYouTube() {
+  var youtubeVideoUrl = "https://www.youtube.com/watch?v=UWq-WsNxqDE&ab_channel=CashNasty";
+  window.open(youtubeVideoUrl, '_blank');
 }
